ab77f8f26f510dae9f2b7dd84fe97bde0df8a472
anhminh0401 <nguyenanhminh04012001@gmail.com>
2023-11-15 16:45:57 +0700

    [auth] fix response

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -5,10 +5,13 @@ import {
   Request,
   Body,
   Post,
+  Res,
 } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { UsersService } from './users.service';
 import { InfoProfileRequest } from './dtos/profile.dto';
+import { Response } from 'express';
+import { ResponseWrapper } from 'src/helper/response-wrapper';
 
 @Controller('users')
 export class UsersController {
@@ -16,13 +19,19 @@ export class UsersController {
 
   @UseGuards(AuthGuard)
   @Get('profile')
-  getProfile(@Request() req) {
-    return this.usersService.getProfile(req.user.userId);
+  async getProfile(@Request() req, @Res() res: Response) {
+    const data = await this.usersService.getProfile(req.user.userId);
+    res.send(new ResponseWrapper(data, null, null));
   }
 
   @UseGuards(AuthGuard)
   @Post('profile')
-  editProfile(@Body() info: InfoProfileRequest, @Request() req) {
-    return this.usersService.editProfile(req.user.userId, info);
+  async editProfile(
+    @Body() info: InfoProfileRequest,
+    @Request() req,
+    @Res() res: Response,
+  ) {
+    const data = await this.usersService.editProfile(req.user.userId, info);
+    res.send(new ResponseWrapper(data, null, null));
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -43,5 +43,6 @@ export class UsersService {
         ...infoUserAdvance,
       });
     });
+    return true;
   };
 }
